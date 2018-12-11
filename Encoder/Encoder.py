a73846929e168ef2722d6fc72d21c8a7d697ea12
Hamani Khalil <hamani_khalil@yahoo.fr>
2018-12-11 10:16:54 +0100

    Encoded 'Purchase' column - Prediction using models : Decision trees, Random forst

--- Encoder/Encoder.py
+++ Encoder/Encoder.py
@@ -20,11 +20,15 @@ class Encoder:
         if isinstance(new_data, str):
             new_data = [new_data]
         
+        res = []
+
         for data in new_data:
             if data not in self.data_to_index:
                 self.data_to_index[data] = self.count
                 self.index_to_data[self.count] = data
                 self.count += 1
+            res.append(self.data_to_index[data])
+        return res
 
     def set_code(self, data, code): # assigne un code a la donnée "data"
         if data not in self.data_to_index:
